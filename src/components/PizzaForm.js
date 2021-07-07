182fd25b19ce44107a2e3ade1b163a8cb2ba614e
jacob <jacobharv00@gmail.com>
2021-07-07 15:23:27 -0600

    done

--- src/components/PizzaForm.js
+++ src/components/PizzaForm.js
@@ -1,11 +1,16 @@
 import React from "react";
 
-function PizzaForm() {
+function PizzaForm({formData, handleInputText, onValueChange, handleChange, handleSubmit}) {
+
+
+
   return (
-    <form onSubmit={null /*handle that submit*/}>
+    <form id={formData.id} onSubmit={handleSubmit}>
       <div className="form-row">
         <div className="col-5">
           <input
+            onChange={handleInputText}
+            value={formData.topping}
             className="form-control"
             type="text"
             name="topping"
@@ -13,7 +18,7 @@ function PizzaForm() {
           />
         </div>
         <div className="col">
-          <select className="form-control" name="size">
+          <select onChange={handleChange} className="form-control" name="size" value={formData.size}>
             <option value="Small">Small</option>
             <option value="Medium">Medium</option>
             <option value="Large">Large</option>
@@ -25,7 +30,9 @@ function PizzaForm() {
               className="form-check-input"
               type="radio"
               name="vegetarian"
+              checked={formData.vegetarian ? 'checked': null}
               value="Vegetarian"
+              onChange={onValueChange}
             />
             <label className="form-check-label">Vegetarian</label>
           </div>
@@ -34,7 +41,9 @@ function PizzaForm() {
               className="form-check-input"
               type="radio"
               name="vegetarian"
+              checked={formData.vegetarian ? null : 'checked'}
               value="Not Vegetarian"
+              onChange={onValueChange}
             />
             <label className="form-check-label">Not Vegetarian</label>
           </div>
